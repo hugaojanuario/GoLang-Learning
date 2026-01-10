4e074bd29a531f805bc57069da211be000b51702
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-01-10 00:36:28 -0300

    multiplos retornos

--- banco/banco.go
+++ banco/banco.go
@@ -12,29 +12,34 @@ type ContaBancaria struct{
 }
 
 func main (){
+	var mensagem string
 	contaHugo := ContaBancaria{"Hugo", 123, 500.00}
 	fmt.Println(contaHugo)
 
 	contaHugo.saldo = sacar(contaHugo, 100)
 	fmt.Println("Saldo Atual:", contaHugo.saldo)
 
-	contaHugo.saldo = depositar(contaHugo,3400)
-	fmt.Println("Saldo Atual:", contaHugo.saldo)
+	mensagem, contaHugo.saldo = depositar(contaHugo,3400)
+	fmt.Println(mensagem, contaHugo.saldo)
 
 
 }
 
-func depositar (c ContaBancaria, valorDeposito float64) float64{
+
+
+func depositar (c ContaBancaria, valorDeposito float64) (string, float64){
 	valor := valorDeposito > 0 && valorDeposito <= 10000
 	if valor{
 		fmt.Println(valor)
 		c.saldo += valorDeposito
-		return c.saldo
+		return "Saldo altual",c.saldo
 	}else{
 		fmt.Println("Saldo nao possivel")
-		return c.saldo
+		return "Saldo atual", c.saldo
 	}
 }
+
+
 
 func sacar (c ContaBancaria, valorSaque float64) float64{
 
