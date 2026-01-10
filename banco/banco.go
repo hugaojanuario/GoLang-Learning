ee4ed81f39ac653093edf73b8a0f3a44c9221d57
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-01-09 22:00:26 -0300

    metodos de saque e deposito

--- banco/banco.go
+++ banco/banco.go
@@ -12,28 +12,41 @@ type ContaBancaria struct{
 }
 
 func main (){
-
 	contaHugo := ContaBancaria{"Hugo", 123, 500.00}
-
 	fmt.Println(contaHugo)
 
-	contaHugo.saldo = sacar(contaHugo, 499)
-
+	contaHugo.saldo = sacar(contaHugo, 100)
 	fmt.Println("Saldo Atual:", contaHugo.saldo)
 
+	contaHugo.saldo = depositar(contaHugo,3400)
+	fmt.Println("Saldo Atual:", contaHugo.saldo)
+
+
+}
+
+func depositar (c ContaBancaria, valorDeposito float64) float64{
+	valor := valorDeposito > 0 && valorDeposito <= 10000
+	if valor{
+		fmt.Println(valor)
+		c.saldo += valorDeposito
+		return c.saldo
+	}else{
+		fmt.Println("Saldo nao possivel")
+		return c.saldo
+	}
 }
 
 func sacar (c ContaBancaria, valorSaque float64) float64{
 
-	if valorSaque > c.saldo {
-		fmt.Println("Saldo insuficiente")
+	valor := valorSaque <= c.saldo && valorSaque > 0
+	if valor{
+		fmt.Println(valor)
+		c.saldo -= valorSaque
+		
+		return c.saldo
+	}else{
+		fmt.Println("Saldo insuficiente e Precisa ser maior que zero")
 		return c.saldo
 	}
 
-	c.saldo -= valorSaque
-	return c.saldo
-
-	
-
-	
 }
